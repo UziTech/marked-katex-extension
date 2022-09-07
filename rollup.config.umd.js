add223d065b9492a4a864c0dbf78e25e5031cc5e
Tony Brix <tony@brix.ninja>
2022-09-07 09:14:01 -0500

    include katex for umd

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,8 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 export default {
   input: 'src/index.js',
   output: {
     name: 'markedKatex',
     file: 'lib/index.umd.js',
     format: 'umd'
-  }
+  },
+  plugins: [nodeResolve()]
 };
