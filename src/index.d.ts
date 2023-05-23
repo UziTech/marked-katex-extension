62159206ee7d04633dadaa8a932fa6c96f0500c3
wtto00 <wtto00@msn.com>
2023-05-23 16:24:49 +0800

    fix: types defined

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,5 @@
-
-import type { marked } from 'marked'
-import type { KatexOptions } from 'katex'
-
 declare module 'marked-katex-extension' {
-  function markedKatex(options: KatexOptions): marked.MarkedExtension
+  function markedKatex(options?: import('katex').KatexOptions): import('marked').marked.MarkedExtension
 
   export default markedKatex
 }
