ee3e903d9029bcdff1a8227aadfa934ecb4a92c5
Rafi-Riday <rafi.riday@gmail.com>
2023-07-05 05:19:54 +0600

    fixed tests

--- spec/index.test.js
+++ spec/index.test.js
@@ -11,6 +11,15 @@ describe('marked-katex-extension', () => {
     'inline katex': `
 This is inline katex: $c = \\pm\\sqrt{a^2 + b^2}$
 `,
+    'inline katex multiple lines': `
+inline katex with multiple lines:
+
+$
+c = \\pm\\sqrt{a^2 + b^2}
+$
+`,
+    'inline katex with another $...$ inside it': `
+inline katex with another \`$...$\` inside it: $\\fcolorbox{red}{aqua}{$\\frac{b}{2}\\sqrt{a^2-\\frac{b^2}{4}}$}$`,
     'block katex': `
 This is block level katex:
 
@@ -18,8 +27,9 @@ $$
 c = \\pm\\sqrt{a^2 + b^2}
 $$
 `,
-    'inline katex more $': `
-This is inline katex: $$c = \\pm\\sqrt{a^2 + b^2}$$
+    'block katex without newline': `
+block katex without newline:
+$$c = \\pm\\sqrt{a^2 + b^2}$$
 `,
     'block katex more $': `
 This is block level katex:
@@ -28,12 +38,11 @@ $$$
 c = \\pm\\sqrt{a^2 + b^2}
 $$$
 `,
-    'block katex 1 $': `
-This is block level katex:
-
-$
-c = \\pm\\sqrt{a^2 + b^2}
-$
+    'block katex with another $...$ inside it': `
+block katex with another \`$...$\` inside it:
+$$
+\\fcolorbox{red}{aqua}{$\\frac{b}{2}\\sqrt{a^2-\\frac{b^2}{4}}$}
+$$
 `
   };
 
