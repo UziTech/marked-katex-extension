518e1258b5bf168fc084b37aacdd40356e97da0f
Tony Brix <tony@brix.ninja>
2023-08-04 23:30:47 -0600

    fix: allow marked v6 (#194)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,1 @@
-export default function markedKatex(options?: import('katex').KatexOptions): import('marked').marked.MarkedExtension
+export default function markedKatex(options?: import('katex').KatexOptions): import('marked').MarkedExtension
