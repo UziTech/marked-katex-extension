e6f63beb1230efa614c6e683376bdace96f7cfb7
CreatorMC <103886337+CreatorMC@users.noreply.github.com>
2023-10-26 14:37:18 +0800

    fix: Allow Chinese punctuation after katex (#269)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import katex from 'katex';
 
-const inlineRule = /^(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:]|$)/;
+const inlineRule = /^(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$)/;
 const blockRule = /^(\${1,2})\n((?:\\[^]|[^\\])+?)\n\1(?:\n|$)/;
 
 export default function(options = {}) {
