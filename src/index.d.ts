3c8f2c5b3eef4fb9fe165f676ff24f5633422ec6
YangFong <yangfong2022@gmail.com>
2024-09-01 21:23:19 +0800

    fix: nonStandard option does not exist

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,1 @@
-export default function markedKatex(options?: import('katex').KatexOptions): import('marked').MarkedExtension;
+export default function markedKatex(options?: import('katex').KatexOptions & { nonStandard?: boolean }): import('marked').MarkedExtension;
